5e049293e26bc07ba93e01df05f362b901dc0f7a
J.J. Green,,, <j.j.green@gmx.co.uk>
2005-06-02 22:38:54 +0000

    Initial revision

---
+++ src/common/svgio.h
@@ -0,0 +1,16 @@
+/*
+  svgio.h
+
+  reads & writes svg gradient files
+
+  J.J. Green 2005
+*/
+
+#ifndef SVGIO_H
+#define SVGIO_H
+
+#include "svg.h"
+
+extern int svg_write(const char*,svg_t*);
+
+#endif
