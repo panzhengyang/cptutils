13d421089aea3977392f2b39353480e986e38738
J.J. Green,,, <j.j.green@gmx.co.uk>
2005-07-19 22:17:56 +0000

    Initial revision

---
+++ src/cptcss/cptcss.c
@@ -0,0 +1,106 @@
+/*
+  cptcss.c - colour text with a cpt file
+
+  J.J. Green 2004
+  $Id: cptcss.c,v 1.2 2004/08/15 23:50:35 jjg Exp $
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "cptio.h"
+
+#include "cptcss.h"
+
+#define NNSTR(x) ((x) ? (x) : "<stdin>")
+
+static int output_css(cpt_t*,const char*);
+
+extern int cptcss(cptcss_opt_t opt)
+{
+  cpt_t *cpt;
+  int    err = 0;
+
+  if ((cpt = cpt_new()) == NULL)
+    fprintf(stderr,"failed to initialise cpt structure\n");
+  else
+    {
+      if (cpt_read(opt.cpt,cpt,0) != 0)
+	{
+	  fprintf(stderr,"failed to read %s\n",NNSTR(opt.cpt));
+	  err = 1;
+	  goto cleanup;
+	}
+
+      if (output_css(cpt,opt.format) != 0)
+	{
+	  fprintf(stderr,"error in css output\n");
+	  err = 1;
+	  goto cleanup;
+	}
+      
+    cleanup:
+  
+      cpt_destroy(cpt);
+    }
+
+  return err;
+}
+
+static int output_css(cpt_t *cpt,const char *format)
+{
+  int n = 0;
+  model_t model;
+  cpt_seg_t *seg;
+  int cllen;
+
+  seg   = cpt->segment;
+  model = cpt->model;
+
+  /* 
+     needs to fit the format string, might get an overflow
+     for sufficiently hostile format string, "arse-%.25" 
+     for example
+   */
+
+  cllen = strlen(format) + 20;
+
+  while (seg)
+    {
+      fill_t fill;
+      rgb_t  rgb;
+      char   hcstr[8]; 
+      char   clstr[cllen];
+
+      if (! fill_eq(seg->lsmp.fill,seg->rsmp.fill))
+	{
+	  fprintf(stderr,"non-discrete cpt file! (use makecpt)\n");
+	  return 1;
+	}
+
+      fill = seg->lsmp.fill;
+
+      if (fill_rgb(fill,model,&rgb) != 0)
+	{
+	  fprintf(stderr,"failed to get rgb!\n");
+	  return 1;
+	}
+      
+      sprintf(hcstr,"#%.2x%.2x%.2x",rgb.red,rgb.blue,rgb.green);
+      
+      if (snprintf(clstr,cllen,format,n) > cllen-1)
+	{
+	  fprintf(stderr,"output truncated (hostile format string?)\n");
+	  return 1;
+	}
+      
+      printf("td.%s { background : %s }\n",clstr,hcstr);
+
+      seg = seg->rseg;
+      n++;
+    }
+
+  return 0;
+}
+
---
+++ src/cptcss/main.c
@@ -0,0 +1,88 @@
+/*
+  main.c for cptcss
+
+  part of the cptutils package
+
+  This program is free software; you can redistribute it
+  and/or modify it under the terms of the GNU General
+  Public License as published by the Free Software
+  Foundation; either version 2 of the License, or (at your
+  option) any later version.
+
+  This program is distributed in the hope that it will be
+  useful, but WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURIGHTE.  See the GNU General Public License
+  for more details.
+
+  You should have received a copy of the GNU General Public
+  License along with this program; if not, write to the
+  Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+  Boston, MA 02111-1307, USA.
+
+  $Id: main.c,v 1.2 2004/08/15 23:50:19 jjg Exp $
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "options.h"
+#include "cptcss.h"
+
+int main(int argc,char** argv)
+{
+  struct gengetopt_args_info info;
+  cptcss_opt_t opt;
+  int err;
+
+  /* use gengetopt */
+
+  if (options(argc,argv,&info) != 0)
+    {
+      fprintf(stderr,"failed to parse command line\n");
+      return EXIT_FAILURE;
+    }
+
+  /* check arguments & transfer to opt structure */ 
+
+  opt.verbose = (info.verbose_given ? true : false);
+
+  if (opt.verbose)
+    printf("This is cptcss (version %s)\n",VERSION);
+  
+  switch (info.inputs_num)
+    {
+    case 0:
+      opt.cpt = NULL;
+      break;
+    case 1:
+      opt.cpt = info.inputs[0];
+      break;
+    default:
+      fprintf(stderr,"Sorry, only one file at a time\n");
+      return EXIT_FAILURE;
+    }
+
+  opt.format = info.format_arg;
+
+  err = cptcss(opt);
+  
+  if (err != 0) fprintf(stderr,"failed (error %i)\n",err);
+
+  if (opt.verbose) printf("done.\n");
+
+  return (err ? EXIT_FAILURE : EXIT_SUCCESS);
+}
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+
