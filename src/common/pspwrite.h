8c7146298d7401e7ca8fbd25299edf967825d4ab
J.J. Green,,, <j.j.green@gmx.co.uk>
2006-09-01 23:16:18 +0000

    *** empty log message ***

--- src/common/pspwrite.h
+++ src/common/pspwrite.h
@@ -3,17 +3,15 @@
 
   writes paintshop pro gradients.
   2006 (c) J.J. Green
-  $Id: pspwrite.h,v 1.3 2006/09/01 22:33:04 jjg Exp jjg $
+  $Id: pspwrite.h,v 1.4 2006/09/01 22:57:30 jjg Exp jjg $
 */
 
 #ifndef PSPWRITE_H
 #define PSPWRITE_H
 
-#include <stdio.h>
-
 #include "psp.h"
 
-extern int psp_write(FILE*,psp_t*);
+extern int psp_write(const char*,psp_t*);
 
 #endif
  
