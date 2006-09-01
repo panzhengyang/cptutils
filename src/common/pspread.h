35684669cd342e940aaf1d3ab816e3ced25bd2ca
J.J. Green,,, <j.j.green@gmx.co.uk>
2006-09-01 20:13:40 +0000

    Initial revision

---
+++ src/common/pspread.h
@@ -0,0 +1,19 @@
+/*
+  pspread.h
+
+  read paintshop pro gradients.
+  2005 (c) J.J. Green
+  $Id: psp.h,v 1.7 2006/08/31 23:24:50 jjg Exp $
+*/
+
+#ifndef PSPREAD_H
+#define PSPREAD_H
+
+#include <stdlib.h>
+
+#include "psp.h"
+
+extern int read_psp(FILE*,psp_grad_t*);
+
+#endif
+ 
---
+++ src/common/pspwrite.h
@@ -0,0 +1,19 @@
+/*
+  pspwrite.h
+
+  writes paintshop pro gradients.
+  2006 (c) J.J. Green
+  $Id: pspread.h,v 1.1 2006/09/01 20:13:40 jjg Exp $
+*/
+
+#ifndef PSPWRITE_H
+#define PSPWRITE_H
+
+#include <stdlib.h>
+
+#include "psp.h"
+
+extern int write_psp(FILE*,psp_grad_t*);
+
+#endif
+ 
