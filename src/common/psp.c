afb585b42605cc10d42aeeb2ab7ff1bd4452e440
J.J. Green,,, <j.j.green@gmx.co.uk>
2006-09-01 23:07:35 +0000

    *** empty log message ***

--- src/common/psp.c
+++ src/common/psp.c
@@ -4,18 +4,36 @@
   paintshop pro gradient structures
   2005 (c) J.J. Green
 
-  $Id: psp.c,v 1.9 2006/09/01 22:32:57 jjg Exp jjg $
+  $Id: psp.c,v 1.10 2006/09/01 22:55:51 jjg Exp jjg $
 */
 
 #include "psp.h"
 
 unsigned char pspmagic[] = "8BGR";
 
-extern int clean_psp(psp_t* psp)
+extern psp_t* psp_new(void)
+{
+  psp_t* psp;
+
+  if ((psp = malloc(sizeof(psp_t))) == NULL)
+    return NULL;
+
+  psp->name    = NULL;
+  psp->rgb.n   = 0;
+  psp->rgb.seg = NULL;
+  psp->op.n    = 0;
+  psp->op.seg  = NULL;
+
+  return psp;
+}
+
+extern void psp_destroy(psp_t* psp)
 {
   free(psp->name);
   free(psp->rgb.seg);
   free(psp->op.seg);
+
+  free(psp);
 
   return 0;
 }
