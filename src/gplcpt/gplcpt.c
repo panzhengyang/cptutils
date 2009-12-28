f095d5236dca459ac17adf4ef0a8f2846d1b8631
J.J. Green,,, <j.j.green@gmx.co.uk>
2009-12-28 21:52:20 +0000

    *** empty log message ***

--- src/gplcpt/gplcpt.c
+++ src/gplcpt/gplcpt.c
@@ -2,14 +2,14 @@
   gplcpt.h
   convert GIMP palette (gpl) to GMT colour table (cpt)
   Copyright (c) J.J. Green 2010
-  $Id$
+  $Id: gplcpt.c,v 1.1 2009/12/28 17:57:10 jjg Exp jjg $
 */
 
 #include <stdio.h>
-//#include <stdlib.h>
 #include <string.h>
-//#include <math.h>
 
+#include "cpt.h"
+#include "cptio.h"
 #include "gplcpt.h"
 
 static int gplcpt_st(gplcpt_opt_t,FILE*);
@@ -41,6 +41,7 @@ extern int gplcpt(gplcpt_opt_t opt)
 
 #define BUFSIZE 1024
 
+static int gplcpt_write(gplcpt_opt_t,cpt_t*);
 static int skipline(const char*);
 
 extern int gplcpt_st(gplcpt_opt_t opt,FILE *st)
@@ -64,6 +65,22 @@ extern int gplcpt_st(gplcpt_opt_t opt,FILE *st)
       return 1;
     }
 
+  /* create cpt struct */
+
+  cpt_t *cpt;
+
+  if ((cpt = cpt_new()) == NULL) return 1;
+
+  cpt->model = rgb;
+
+  /* set bg/fg/nan values */
+  
+  cpt->fg.type = cpt->bg.type = cpt->nan.type = colour;
+
+  cpt->bg.u.colour.rgb  = opt.bg;
+  cpt->fg.u.colour.rgb  = opt.fg;
+  cpt->nan.u.colour.rgb = opt.nan;
+
   /* get next non-comment line */
 
   do
@@ -81,12 +98,16 @@ extern int gplcpt_st(gplcpt_opt_t opt,FILE *st)
 	  printf("GIMP palette %s\n",name);
 	}
 
+      strncpy(cpt->name,name,CPT_NAME_LEN);
+
       /* skip to next non-comment */
 
       do
 	if (fgets(buf,BUFSIZE,st) == NULL) return 1;
       while (skipline(buf));
     }
+  else
+    strncpy(cpt->name,"unnamed",CPT_NAME_LEN);
 
   /* see if it is columns line */
 
@@ -108,6 +129,11 @@ extern int gplcpt_st(gplcpt_opt_t opt,FILE *st)
 
   /* now at the rgb data we hope */
 
+  fill_t fill;
+  fill.type = colour;
+
+  size_t n = 0;
+
   while (1)
     {
       int r,g,b;
@@ -118,23 +144,103 @@ extern int gplcpt_st(gplcpt_opt_t opt,FILE *st)
 	  return 1;
 	} 
 
-      /* FIXME */
+      fill.u.colour.rgb.red   = r;
+      fill.u.colour.rgb.green = g;
+      fill.u.colour.rgb.blue  = b;
 
-      printf("%3i %3i %3i\n",r,g,b);
+      cpt_seg_t* seg;
+
+      if ((seg = cpt_seg_new()) == NULL)
+	{
+	  fprintf(stderr,"failed to create new segment\n");
+	  return 1;
+	}
+
+      seg->lsmp.val  = n;
+      seg->lsmp.fill = fill;
+
+      seg->rsmp.val  = n+1;
+      seg->rsmp.fill = fill;
+
+      if (cpt_append(seg,cpt) != 0)
+	{
+	  fprintf(stderr,"failed append of segment %i\n",(int)n);
+	  return 1;
+	}
+
+      n++;
 
       do
 	if (fgets(buf,BUFSIZE,st) == NULL)
 	  {
-	    /* end of data ... */
+	    if (opt.verbose)
+	      {
+		printf("read %i RGB triples\n",n);
+	      }
 
-	    printf("reading finished\n");
-
-	    return 0;
+	    return gplcpt_write(opt,cpt);
 	  }
       while (skipline(buf));
     }
 
   return 1;
+}
+
+static int cpt_step_optimise(cpt_t*);
+
+static int gplcpt_write(gplcpt_opt_t opt,cpt_t *cpt)
+{
+  if (cpt_step_optimise(cpt) != 0)
+    {
+      fprintf(stderr,"failed optimise\n");
+      return 1;
+    }
+
+  if (opt.verbose)
+    {
+      printf("converted to %i segment cpt\n",cpt_nseg(cpt));
+    }
+
+  return cpt_write(opt.file.output,cpt);
+}
+
+/*
+  aggregates segments which have the same fill,
+  assuming that the input consists of piecewise
+  constant segments (as here) 
+*/
+
+static int cpt_step_optimise(cpt_t* cpt)
+{
+  cpt_seg_t *left,*right;
+
+  left  = cpt->segment;
+  right = left->rseg; 
+
+  while (right)
+    {
+      if (fill_eq(left->rsmp.fill, right->lsmp.fill))
+	{
+	  cpt_seg_t* dead = right;
+
+	  if ((right = dead->rseg) != NULL)
+	    {
+	      right->lseg = left;
+	      left->rsmp.val = right->lsmp.val;
+	    }
+
+	  left->rseg = right;
+	  
+	  cpt_seg_destroy(dead);
+	}
+      else
+	{
+	  left  = left->rseg;
+	  right = right->rseg;
+	}
+    }
+
+  return 0;
 }
 
 /* 
