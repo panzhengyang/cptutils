fb947ef12e003b144217ab75909623129263be1f
J.J. Green,,, <j.j.green@gmx.co.uk>
2010-11-01 19:45:12 +0000

    *** empty log message ***

--- src/common/gptwrite.c
+++ src/common/gptwrite.c
@@ -5,7 +5,7 @@
   a file or stream.
 
   J.J.Green 2010
-  $Id: gptwrite.c,v 1.2 2010/11/01 19:22:29 jjg Exp jjg $
+  $Id: gptwrite.c,v 1.3 2010/11/01 19:43:40 jjg Exp jjg $
 */
 
 #include <stdlib.h>
@@ -38,7 +38,7 @@ extern int gpt_write(const char* file,gpt_t* gpt)
   time_t t = time(NULL);
 
   fprintf(st,"# Gnuplot colour map\n");
-  fprintf(st,"# cptutils %s, %s\n",VERSION,ctime(&t));
+  fprintf(st,"# cptutils %s, %s",VERSION,ctime(&t));
   
   for (i=0 ; i<n ; i++)
     {
