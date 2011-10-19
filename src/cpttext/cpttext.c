f30d87b25880176e963a880be69c3cb0c68711fc
J.J. Green,,, <j.j.green@gmx.co.uk>
2011-10-19 22:15:00 +0000

    *** empty log message ***

--- src/cpttext/cpttext.c
+++ src/cpttext/cpttext.c
@@ -2,7 +2,7 @@
   cpttext.c - colour text with a cpt file
 
   J.J. Green 2004
-  $Id: cpttext.c,v 1.1 2004/04/12 23:43:01 jjg Exp jjg $
+  $Id: cpttext.c,v 1.2 2004/08/15 23:50:35 jjg Exp jjg $
 */
 
 #include <stdlib.h>
@@ -30,19 +30,19 @@ extern int cpttext(cpttext_opt_t opt)
   int    err = 0;
 
   if ((cpt = cpt_new()) == NULL)
-    fprintf(stderr,"failed to initialise cpt structure\n");
+    fprintf(stderr, "failed to initialise cpt structure\n");
   else
     {
-      if (cpt_read(opt.cpt,cpt,0) != 0)
+      if (cpt_read(opt.cpt, cpt, 0) != 0)
 	{
-	  fprintf(stderr,"failed to read %s\n",NNSTR(opt.cpt));
+	  fprintf(stderr, "failed to read %s\n", NNSTR(opt.cpt));
 	  err = 1;
 	  goto cleanup;
 	}
 
-      if (cpt_zrange(cpt,z) != 0)
+      if (cpt_zrange(cpt, z) != 0)
 	{
-	  fprintf(stderr,"failed to get zrange\n");
+	  fprintf(stderr, "failed to get zrange\n");
 	  err = 1;
 	  goto cleanup;
 	}
@@ -50,22 +50,22 @@ extern int cpttext(cpttext_opt_t opt)
       switch (opt.format)
 	{
 	case html:
-	  if (output_html(opt.text,cpt,z) != 0)
+	  if (output_html(opt.text, cpt, z) != 0)
 	    {
-	      fprintf(stderr,"error in html output\n");
+	      fprintf(stderr, "error in html output\n");
 	      err = 1;
 	      goto cleanup;
 	    }
 	  break;
 	  
 	case css:
-	  fprintf(stderr,"css output not yet implemented\n");
+	  fprintf(stderr, "css output not yet implemented\n");
 	  err = 1;
 	  goto cleanup;
 	  break;
 	  
 	default:
-	  fprintf(stderr,"strange output specified\n");
+	  fprintf(stderr, "strange output specified\n");
 	  err = 1;
 	  goto cleanup;
 	}
@@ -78,7 +78,7 @@ extern int cpttext(cpttext_opt_t opt)
   return err;
 }
 
-static int output_html(char *text,cpt_t *cpt,double *zrng)
+static int output_html(char *text, cpt_t *cpt, double *zrng)
 {
   int     n,i;
   double  dz,z;
@@ -91,7 +91,7 @@ static int output_html(char *text,cpt_t *cpt,double *zrng)
   model = cpt->model;
 
 #ifdef DEBUG
-  printf("%f %f\n",zrng[0],zrng[1]);
+  printf("%f %f\n",zrng[0], zrng[1]);
 #endif
 
   for (i=0 ; i<n ; i++)
@@ -103,20 +103,20 @@ static int output_html(char *text,cpt_t *cpt,double *zrng)
 
       z = zrng[0] + (0.5 + i)*dz;
 
-      if (cpt_zfill(cpt,z,&fill) != 0)
+      if (cpt_zfill(cpt, z, &fill) != 0)
 	{
-	  fprintf(stderr,"error getting fill for z = %f\n",z);
+	  fprintf(stderr, "error getting fill for z = %f\n", z);
 	  return 1;
 	}
 
-      if (fill_rgb(fill,model,&rgb) != 0)
+      if (fill_rgb(fill, model, &rgb) != 0)
 	{
-	  fprintf(stderr,"error rgb values for fill (z = %f)\n",z);
+	  fprintf(stderr, "error rgb values for fill (z = %f)\n", z);
 	  return 1;
 	}
 
 #ifdef DEBUG
-      printf("%f -> (%i %i %i) ",z,rgb.red,rgb.blue,rgb.green);
+      printf("%f -> (%i %i %i) ", z, rgb.red, rgb.green, rgb.blue);
 #endif
 
       switch (c)
@@ -127,8 +127,8 @@ static int output_html(char *text,cpt_t *cpt,double *zrng)
 	  printf("%c",c);
 	  break;
 	default:
-	  sprintf(hcstr,"#%.2x%.2x%.2x",rgb.red,rgb.blue,rgb.green);
-	  printf("<font color=\"%s\">%c</font>",hcstr,c);
+	  sprintf(hcstr, "#%.2x%.2x%.2x", rgb.red, rgb.green, rgb.blue);
+	  printf("<font color=\"%s\">%c</font>", hcstr, c);
 	}
     }
 
