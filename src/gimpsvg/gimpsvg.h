b7e3c43bd2dbf7d5ce7d61433d5bdb57966d5f2d
J.J. Green,,, <j.j.green@gmx.co.uk>
2011-11-10 23:56:24 +0000

    *** empty log message ***

--- src/gimpsvg/gimpsvg.h
+++ src/gimpsvg/gimpsvg.h
@@ -2,28 +2,24 @@
   gimpsvg.h
 
   (c) J.J.Green 2011
-  $Id: gimpsvg.h,v 1.2 2004/02/24 18:36:23 jjg Exp jjg $
+  $Id: gimpsvg.h,v 1.3 2011/11/10 23:34:27 jjg Exp jjg $
 */
 
 #ifndef GIMPSVG_H
 #define GIMPSVG_H
 
+#include <stdbool.h>
+#include <stdlib.h>
+
 #include "colour.h"
 
 typedef struct 
 {
-  unsigned int verbose    : 1;
-  unsigned int reverse    : 1;
-  
-  rgb_t  fg,bg,nan,trans;
-  
-  int samples;
-  
-  double min;
-  double max;
-  
+  bool verbose, reverse;
+  rgb_t fg, bg, nan;
+  size_t samples;
+  double min, max;
   double tol;
-
 } gimpsvg_opt_t;
 
 extern int gimpsvg(const char*, const char*, gimpsvg_opt_t);
