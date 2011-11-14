d5151fa9724080e0a93c19b5fb07bcd4609eed71
J.J. Green,,, <j.j.green@gmx.co.uk>
2011-11-14 16:03:14 +0000

    *** empty log message ***

--- src/svgx/svgx.h
+++ src/svgx/svgx.h
@@ -2,11 +2,13 @@
   svgx.h
 
   (c) J.J.Green 2004
-  $Id: svgx.h,v 1.10 2011/11/06 21:25:16 jjg Exp jjg $
+  $Id: svgx.h,v 1.11 2011/11/13 12:12:52 jjg Exp jjg $
 */
 
 #ifndef SVGX_H
 #define SVGX_H
+
+#include <stdbool.h>
 
 #include "colour.h"
 
@@ -21,24 +23,27 @@ typedef enum {
   type_png 
 } svgx_type_t;
 
+typedef enum {
+  job_first,
+  job_list,
+  job_named,
+  job_all
+} svgx_job_t;
+
 typedef struct
 {
-  int verbose;
-  int permissive;
-  int debug;
-  int all;
-  int first;
-  int list;
-  int width,height;
+  svgx_type_t type; 
+  svgx_job_t job;
+  bool verbose, permissive, debug;
+  size_t width,height;
   char *name;
   rgb_t fg,bg,nan;
   rgb_t alpha;
-  svgx_type_t type; 
   struct
   {
     char *file;
     FILE *stream;;
-  } input,output;
+  } input, output;
 } svgx_opt_t;
 
 extern int svgx(svgx_opt_t);
