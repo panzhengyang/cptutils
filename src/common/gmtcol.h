3ce2dbc779f098df5d10632b136491fe4d7fc48d
J.J. Green <j.j.green@gmx.co.uk>
2013-10-29 21:15:44 +0000

    added gmtcol colour lookup (gperf)

---
+++ src/common/gmtcol.h
@@ -0,0 +1,18 @@
+/*
+  gmtcol.h
+
+  Copyright (c) J.J. Green 2013
+*/
+
+#ifndef GMTCOL_H
+#define GMTCOL_H
+
+struct gmtcol_t 
+{
+  char* name; 
+  char r,g,b; 
+};
+
+extern struct gmtcol_t* stdcol(const char*);
+
+#endif
