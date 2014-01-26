3b40f3141046d683f542d05882798161681493ca
J.J. Green <j.j.green@gmx.co.uk>
2014-01-26 20:30:46 +0000

    keyword parser for pssvg

---
+++ src/pssvg/grd5_type.c
@@ -0,0 +1,57 @@
+/*
+  grd5_type.c
+  Copyright (c) J.J. Green 2014
+*/
+
+#include "grd5_type.h"
+
+#include <string.h>
+
+/*
+  return codes of grd5 keywords, just a switch on
+  the first character and a strncmp() of the whole
+  string -- one could use gperf here but there are
+  only a few keywords to test.
+*/
+
+extern int grd5_type(const char* str)
+{
+  switch (str[0])
+    {
+    case 'b':
+      if (strncmp("bool", str, 4) == 0)
+	return TYPE_BOOL;
+      break;
+    case 'd':
+      if (strncmp("doub", str, 4) == 0)
+	return TYPE_DOUBLE;
+      break;
+    case 'l':
+      if (strncmp("long", str, 4) == 0)
+	return TYPE_LONG;
+      break;
+    case 'O':
+      if (strncmp("Objc", str, 4) == 0)
+	return TYPE_OBJECT;
+      break;
+    case 'T':
+      if (strncmp("TEXT", str, 4) == 0)
+	return TYPE_TEXT;
+      break;
+    case 'p':
+      if (strncmp("patt", str, 4) == 0)
+	return TYPE_PATTERN;
+      break;
+    case 'U':
+      if (strncmp("Untf", str, 4) == 0)
+	return TYPE_UntF;
+      break;
+    case 'V':
+      if (strncmp("VlLs", str, 4) == 0)
+	return TYPE_VlLs;
+      break;
+    default:
+      return TYPE_UNKNOWN;
+    }
+  return TYPE_UNKNOWN;
+}
---
+++ src/pssvg/grd5_type.h
@@ -0,0 +1,22 @@
+/*
+  grd5_type.h
+  Copyright (c) J.J. Green 2014
+*/
+
+#ifndef GRD5_TYPE_H
+#define GRD5_TYPE_H
+
+#define TYPE_UNKNOWN     0
+#define TYPE_PATTERN     1
+#define TYPE_DESCRIPTIN  2
+#define TYPE_VlLs        3
+#define TYPE_TEXT        4
+#define TYPE_OBJECT      5
+#define TYPE_UntF        6
+#define TYPE_BOOL        7
+#define TYPE_LONG        8
+#define TYPE_DOUBLE      9
+
+extern int grd5_type(const char*);
+
+#endif
