7eb38e0253155d62c8f2f8b95adb9d0f23a49e31
J.J. Green <j.j.green@gmx.co.uk>
2014-05-02 00:09:06 +0100

    acceptance tests for avlcpt

--- src/avlcpt/accept.sh
+++ src/avlcpt/accept.sh
@@ -2,8 +2,17 @@
 
 source $2/accept-header.sh
 
-assert "echo test" "test"
-assert "echo test" "test"
-assert "echo test" "test"
+# 96004SP.avl is in the test directory, but does not
+# contain a gradient
+
+for base in ddvelev1a ddvelev9c
+do
+    fixture="$TESTFIX/avl/$base.avl"
+    output="$base.cpt"
+    reference="accept/$output"
+    assert_raises "./avlcpt -n20/0/0 -v -o $output $fixture" 0
+    assert "diff -I cptutils $output $reference"
+    rm $output
+done
 
 source accept-footer.sh
--- src/test/accept-header.sh
+++ src/test/accept-header.sh
@@ -2,6 +2,7 @@ set -e
 
 NAME=$1
 TESTLIB=$2
+TESTFIX=$3
 
 export PATH=$TESTLIB:$PATH
 source assert.sh
