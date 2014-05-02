54e22764f174b6a277898d3455daab5e6ea13794
J.J. Green <j.j.green@gmx.co.uk>
2014-05-02 21:54:36 +0100

    acceptance tests for cptcat

---
+++ src/cptcat/accept.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source $2/accept-header.sh
+
+base1=tpsfhm
+base2=pakistan
+
+fix1="$TESTFIX/cpt/$base1.cpt"
+fix2="$TESTFIX/cpt/$base2.cpt"
+
+out12="$base1-$base2.cpt"
+out21="$base2-$base1.cpt"
+
+reference="accept/$out21"
+
+assert_raises "./cptcat -o $out12 $fix1 $fix2" 0
+assert_raises "./cptcat -o $out21 $fix2 $fix1" 0
+assert "cpt-equal -b $out12 $out21" true
+assert "cpt-equal -b $out12 $reference" true
+
+rm -f $out12 $out21
+
+source accept-footer.sh
