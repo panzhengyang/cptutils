3f4a1b1bfbabd995168d991cd05a668e43bac58f
J.J. Green <j.j.green@gmx.co.uk>
2014-05-02 23:39:33 +0100

    acceptance tests for cptclip

---
+++ src/cptclip/accept.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source $2/accept-header.sh
+
+base="RdBu_10"
+
+fixture="$TESTFIX/cpt/$base.cpt"
+
+# clipping by z-range
+
+assert_raises "./cptclip -o $base-cc.cpt -R 2.5/5 $fixture" 0
+assert "cpt-equal -b $base-cc.cpt accept/$base-cc.cpt" true
+
+# clipping by segment number
+
+assert_raises "./cptclip -o $base-cd.cpt -s -R 2/5 $fixture" 0
+assert "cpt-equal -b $base-cd.cpt accept/$base-cd.cpt" true
+
+rm -f "$base-cc.cpt" "$base-cd.cpt"
+
+source accept-footer.sh
