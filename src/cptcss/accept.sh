ae93eda19b21a6f7868e66d7c6538bcd6218dfab
J.J. Green <j.j.green@gmx.co.uk>
2014-05-04 18:32:10 +0100

    acceptance tests for cptcss

---
+++ src/cptcss/accept.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source $2/accept-setup.sh
+
+base="GMT_haxby"
+cpt="$base.cpt"
+css1="$base-01.css"
+css2="$base-02.css"
+
+fixture="$TESTFIX/cpt/$cpt"
+
+# default 
+
+assert_raises "./cptcss $fixture > $css1" 0
+assert "css-equal -b $css1 accept/$css1" true
+
+# custom label
+
+assert_raises "./cptcss -f 'level-%.2' $fixture > $css2" 0
+assert "css-equal -b $css2 accept/$css2" true
+
+rm -f $css1 $css2
+
+source accept-teardown.sh
