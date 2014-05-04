f448137f13c3635906f9f11e8e33c23bb11f60d7
J.J. Green <j.j.green@gmx.co.uk>
2014-05-04 20:58:21 +0100

    acceptance tests for pspcpt

---
+++ src/pspsvg/accept.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source $2/accept-setup.sh
+
+fixture_dir="$TESTFIX/grd3"
+
+for n in `seq 1 4` 
+do
+    base="test.$n" 
+    jgd="$base.jgd"
+    svg="$base.svg"
+    assert_raises "./pspsvg -p -o $svg $fixture_dir/$jgd" 0
+    assert "svg-equal -b $svg accept/$svg" true
+    rm -f $svg
+done
+
+source accept-teardown.sh
