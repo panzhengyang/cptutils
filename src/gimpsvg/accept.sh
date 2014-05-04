85530be32c07f4d9f5256c018bea5c36e14dde52
J.J. Green <j.j.green@gmx.co.uk>
2014-05-04 23:42:04 +0100

    fixed a deliberate travis fail

--- src/gimpsvg/accept.sh
+++ src/gimpsvg/accept.sh
@@ -8,7 +8,7 @@ svg="$base.svg"
 fixtures="$TESTFIX/ggr"
 
 assert_raises \
-    "export GIMP_GRADIENTS=$fxtures ; ./gimpsvg -p -o $svg $base" 0
+    "export GIMP_GRADIENTS=$fixtures ; ./gimpsvg -p -o $svg $base" 0
 assert "svg-equal -b $svg accept/$svg" true
 
 rm -f $svg
