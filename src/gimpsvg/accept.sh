ac44fb35d7f7f5c1fd5b40ab08824522054650bd
J.J. Green <j.j.green@gmx.co.uk>
2014-05-04 23:39:05 +0100

    acceptance tests fail harder

--- src/gimpsvg/accept.sh
+++ src/gimpsvg/accept.sh
@@ -5,10 +5,10 @@ source $2/accept-setup.sh
 base="Sunrise"
 svg="$base.svg"
 
-fixture_dir="$TESTFIX/ggr"
+fixtures="$TESTFIX/ggr"
 
 assert_raises \
-    "export GIMP_GRADIENTS=$fixtrure_dir ; ./gimpsvg -p -o $svg $base" 0
+    "export GIMP_GRADIENTS=$fxtures ; ./gimpsvg -p -o $svg $base" 0
 assert "svg-equal -b $svg accept/$svg" true
 
 rm -f $svg
