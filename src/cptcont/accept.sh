c7a65c6b92022c07ccbc07add91258be5692a810
J.J. Green <j.j.green@gmx.co.uk>
2014-05-04 16:35:43 +0100

    acceptance tests for cptcont

---
+++ src/cptcont/accept.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source $2/accept-setup.sh
+
+base="RdBu_10"
+cpt_cont="$base-cont.cpt"
+cpt_ac="$base-ac.cpt"
+
+fixture="$TESTFIX/cpt/$base.cpt"
+
+# continuous cpt
+
+assert_raises "./cptcont -o $cpt_cont -m $fixture" 0
+assert "cpt-equal -b $cpt_cont accept/$cpt_cont" true
+
+# almost continuous
+
+assert_raises "./cptcont -o $cpt_ac -m -p 60 $fixture" 0
+assert "cpt-equal -b $cpt_ac accept/$cpt_ac" true
+
+rm -f $cpt_cont $cpt_ac
+
+source accept-teardown.sh
