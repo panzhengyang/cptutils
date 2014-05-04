75121da1de94333c57a67551592f53649d892c98
J.J. Green <j.j.green@gmx.co.uk>
2014-05-04 21:17:56 +0100

    acceptance tests for pssvg

---
+++ src/pssvg/accept.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source $2/accept-setup.sh
+
+fixtures="$TESTFIX/grd5"
+
+bases=$(cat <<EOF
+my-custom-gradient-3-rgb
+my-custom-gradient-3-hsb
+my-custom-gradient-3-lab
+neverhappens-titi-montoya
+EOF
+)
+
+for base in $bases 
+do
+    grd="$base.grd"
+    svg="$base.svg"
+    assert_raises "./pssvg -o $svg $fixtures/$grd" 0
+    assert "svg-equal -b $svg accept/$svg" true
+    rm -f $svg
+done
+
+source accept-teardown.sh
