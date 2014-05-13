351f42b81c3bef77e0c20ea5193d1b6efe521ca5
J.J. Green <j.j.green@gmx.co.uk>
2014-05-13 22:05:49 +0100

    unit tests for files.c

--- src/unit/tests_files.c
+++ src/unit/tests_files.c
@@ -3,6 +3,9 @@
   Copyright (c) J.J. Green 2014
 */
 
+#include <sys/stat.h>
+#include <stdio.h>
+
 #include <files.h>
 #include "tests_files.h"
 
@@ -15,11 +18,24 @@ CU_TestInfo tests_files[] =
 
 extern void test_files_is_readable(void)
 {
+  char *path = tmpnam(NULL);
+  FILE *st = fopen(path, "w");
+  fprintf(st, "hello\n");
+  fclose(st);
 
+  chmod(path, 0);
+  CU_ASSERT( ! is_readable(path) );
+
+  chmod(path, S_IRUSR | S_IWUSR);
+  CU_ASSERT( is_readable(path) );
+
+  unlink(path);
+  CU_ASSERT(! is_readable(path));
 }
 
 extern void test_files_is_absolute_filename(void)
 {
-
+  CU_ASSERT( is_absolute_filename("/very/much/so") );
+  CU_ASSERT( ! is_absolute_filename("not/at/all") );
 }
 
