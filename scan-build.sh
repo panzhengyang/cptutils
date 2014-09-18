1a6e5e1563d56b9d3c59df21bf44ab08e1095e6e
J.J. Green <j.j.green@gmx.co.uk>
2014-09-18 20:51:10 +0100

    change of scan-build results directory

--- scan-build.sh
+++ scan-build.sh
@@ -3,7 +3,11 @@
 # run scan-build (clang) static analysis and dump the
 # results in the parent-directory
 
-SCAN_RESULTS="../clang-static"
+SCAN_RESULTS="/tmp"
 
+if [ -e Makefile ]
+then
+    make spotless
+fi
 scan-build -v ./configure
 scan-build -v -o $SCAN_RESULTS make
