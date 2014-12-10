2dcac238a2b921e8bda8f6c678f2c95fe1603ff3
J.J. Green <j.j.green@gmx.co.uk>
2014-12-10 23:22:08 +0000

    test fixture for bad-whitespace problem with cpt parse

--- src/cptsvg/accept.sh
+++ src/cptsvg/accept.sh
@@ -2,14 +2,15 @@
 
 source $2/accept-setup.sh
 
-base="subtle"
-svg="$base.svg"
+for base in subtle bad-whitespace
+do
+    svg="$base.svg"
+    fixture="$TESTFIX/cpt/$base.cpt"
 
-fixture="$TESTFIX/cpt/$base.cpt"
+    assert_raises "./cptsvg --geometry 500x100 -o $svg $fixture" 0
+    assert "equal-svg $svg accept/$svg" true
 
-assert_raises "./cptsvg --geometry 500x100 -o $svg $fixture" 0
-assert "equal-svg $svg accept/$svg" true
-
-rm -f $svg
+    rm -f $svg
+done
 
 source accept-teardown.sh
