60e44e0af1e888b29ce0c8882d3a65518a2c8a86
J.J. Green <j.j.green@gmx.co.uk>
2015-04-16 21:03:32 +0100

    regression test for avlcpt infinite-loop bug

--- src/avlcpt/accept.sh
+++ src/avlcpt/accept.sh
@@ -15,4 +15,14 @@ do
     rm $output
 done
 
+# this is an image file which, up to version 1.57, sent avlcpt
+# into an infinite loop (in the flex scanner)
+
+base='not-an-avl'
+fixture="$TESTFIX/avl/$base.avl"
+output="$base.cpt"
+assert_raises "./avlcpt -n20/0/0 -v -o $output $fixture" 1
+assert_raises "[ ! -e $output ]" 0
+rm -f $output
+
 source accept-teardown.sh
