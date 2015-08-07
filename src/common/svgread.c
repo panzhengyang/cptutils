f98623cd1123d64641a342d123aa866540f75ac6
J.J. Green <j.j.green@gmx.co.uk>
2015-08-08 00:17:10 +0100

    handle rgba colours in svgread.c

--- src/common/svgread.c
+++ src/common/svgread.c
@@ -1,8 +1,8 @@
 /*
   svgread.c
 
-  reads svg gradient files - a list of svg_t structs is 
-  returned (since a single svg file may contain several 
+  reads svg gradient files - a list of svg_t structs is
+  returned (since a single svg file may contain several
   svg gradients)
 
   J.J. Green 2005, 2015
@@ -45,7 +45,7 @@ static void error_handler(void *user_data, xmlErrorPtr error)
     }
 }
 
-static int svg_read_lingrads(xmlNodeSetPtr,svg_list_t*);
+static int svg_read_lingrads(xmlNodeSetPtr, svg_list_t*);
 
 extern int svg_read(const char* file, svg_list_t* list)
 {
@@ -53,7 +53,7 @@ extern int svg_read(const char* file, svg_list_t* list)
   xmlDocPtr doc;
 
   xmlInitParser();
-  xmlSetStructuredErrorFunc(NULL, error_handler); 
+  xmlSetStructuredErrorFunc(NULL, error_handler);
 
   /* Load XML document */
 
@@ -64,11 +64,11 @@ extern int svg_read(const char* file, svg_list_t* list)
     }
   else
     {
-      xmlXPathContextPtr xpc; 
+      xmlXPathContextPtr xpc;
 
       /* Create xpath evaluation context */
-   
-      if ((xpc = xmlXPathNewContext(doc)) == NULL) 
+
+      if ((xpc = xmlXPathNewContext(doc)) == NULL)
 	{
 	  btrace("error: unable to create new XPath context");
 	  err = 1;
@@ -79,7 +79,7 @@ extern int svg_read(const char* file, svg_list_t* list)
 	    register the svg namespace -- often svg images use the default
 	    svg namespace
 
-              <svg xmlns="http://www.w3.org/2000/svg" ...> 
+              <svg xmlns="http://www.w3.org/2000/svg" ...>
 
 	    so that each unprefixed child element has an implicit svg: prefix,
 	    and this needs to be accounted for in the xpath specification.
@@ -88,11 +88,11 @@ extern int svg_read(const char* file, svg_list_t* list)
 	    context.
 	  */
 
-	  const unsigned char 
+	  const unsigned char
 	    prefix[] = "svg",
 	    href[]   = "http://www.w3.org/2000/svg";
-	  
-	  if (xmlXPathRegisterNs(xpc,prefix,href) != 0)
+
+	  if (xmlXPathRegisterNs(xpc, prefix, href) != 0)
 	    {
 	      btrace("namespace error for %s:%s", prefix, href);
 	      err = 1;
@@ -108,41 +108,41 @@ extern int svg_read(const char* file, svg_list_t* list)
 		are looking for are the stops
 	      */
 
-	      const xmlChar xpe[] = 
+	      const xmlChar xpe[] =
 		"//linearGradient | "
 		"//svg:linearGradient | "
 		"//radialGradient | "
 		"//svg:radialGradient";
 
-	      xmlXPathObjectPtr xpo; 
+	      xmlXPathObjectPtr xpo;
 
 	      /* evaluate xpath expression */
-	  
-	      if ((xpo = xmlXPathEvalExpression(xpe, xpc)) == NULL) 
+
+	      if ((xpo = xmlXPathEvalExpression(xpe, xpc)) == NULL)
 		{
-		  btrace("unable to evaluate xpath expression %s",xpe);
+		  btrace("unable to evaluate xpath expression %s", xpe);
 		  err = 1;
 		}
 	      else
 		{
 		  /* process results */
-		  
-		  err = svg_read_lingrads(xpo->nodesetval,list);
-		 		  
+
+		  err = svg_read_lingrads(xpo->nodesetval, list);
+
 		  xmlXPathFreeObject(xpo);
 		}
 	    }
-	  xmlXPathFreeContext(xpc);  
+	  xmlXPathFreeContext(xpc);
 	}
-      xmlFreeDoc(doc); 
+      xmlFreeDoc(doc);
     }
 
   xmlCleanupParser();
-    
+
   return err;
 }
 
-static int svg_read_lingrad(xmlNodePtr, svg_t*); 
+static int svg_read_lingrad(xmlNodePtr, svg_t*);
 
 /*
   the first argument is a list of pointers to svg linearGradient
@@ -153,23 +153,23 @@ static int svg_read_lingrad(xmlNodePtr, svg_t*);
   and call svg_read_lingrad()
 */
 
-static int svg_read_lingrads(xmlNodeSetPtr nodes, svg_list_t* list) 
+static int svg_read_lingrads(xmlNodeSetPtr nodes, svg_list_t* list)
 {
-  int   size,i;
-    
+  int   size, i;
+
   size = (nodes ? nodes->nodeNr : 0);
 
-  for (i = 0 ; i<size ; ++i) 
+  for (i = 0 ; i<size ; ++i)
     {
       int err;
-      xmlChar *id,*href;
+      xmlChar *id, *href;
       xmlNodePtr cur;
       svg_t *svg;
 
-      cur = nodes->nodeTab[i];   	    
+      cur = nodes->nodeTab[i];
 
       assert(cur);
-	
+
       if (cur->type != XML_ELEMENT_NODE)
 	{
 	  btrace("bad svg: gradient is not a node!");
@@ -182,10 +182,10 @@ static int svg_read_lingrads(xmlNodeSetPtr nodes, svg_list_t* list)
 	we skip anything with a reference
       */
 
-      if ((href = xmlGetProp(cur,(unsigned char*)"href")) != NULL)
+      if ((href = xmlGetProp(cur, (unsigned char*)"href")) != NULL)
 	{
 	  xmlFree(href);
-	  continue; 
+	  continue;
 	}
 
       /*
@@ -194,10 +194,10 @@ static int svg_read_lingrads(xmlNodeSetPtr nodes, svg_list_t* list)
 	svg from the svg_list
       */
 
-      if ((id = xmlGetProp(cur,(xmlChar *)"id")) == NULL)
+      if ((id = xmlGetProp(cur, (xmlChar *)"id")) == NULL)
 	{
 	  btrace("gradient has no id attribute, skipping");
-	  continue; 
+	  continue;
 	}
 
       if ((svg = svg_list_svg(list)) == NULL)
@@ -212,7 +212,7 @@ static int svg_read_lingrads(xmlNodeSetPtr nodes, svg_list_t* list)
 
       if (xmlStrPrintf(svg->name,
 		       SVG_NAME_LEN-1,
-		       (xmlChar *)"%s",id) >= SVG_NAME_LEN)
+		       (xmlChar *)"%s", id) >= SVG_NAME_LEN)
 	{
 	  btrace("long gradient name truncated!");
 	}
@@ -221,7 +221,7 @@ static int svg_read_lingrads(xmlNodeSetPtr nodes, svg_list_t* list)
 
       /* now process the gradient */
 
-      err = svg_read_lingrad(cur,svg);
+      err = svg_read_lingrad(cur, svg);
 
       if (err) return err;
     }
@@ -229,17 +229,17 @@ static int svg_read_lingrads(xmlNodeSetPtr nodes, svg_list_t* list)
   return 0;
 }
 
-static int parse_style(const char*,rgb_t*,double*);
-static int parse_offset(const char*,double*);
-static int parse_colour(char*,rgb_t*,double*);
-static int parse_opacity(char*,double*);
+static int parse_style(const char*, rgb_t*, double*);
+static int parse_offset(const char*, double*);
+static int parse_colour(char*, rgb_t*, double*);
+static int parse_opacity(char*, double*);
 
-static int svg_read_lingrad(xmlNodePtr lgrad, svg_t* svg) 
+static int svg_read_lingrad(xmlNodePtr lgrad, svg_t* svg)
 {
-  xmlNode *nodes,*node;
+  xmlNode *nodes, *node;
 
   /*
-    here is where you would read the relevant attributes 
+    here is where you would read the relevant attributes
     from the gradient, but we are not interested in them,
     we just want the nodes.
   */
@@ -252,11 +252,11 @@ static int svg_read_lingrad(xmlNodePtr lgrad, svg_t* svg)
       xmlChar *offset;
 
       if (node->type != XML_ELEMENT_NODE) continue;
-	
+
       if (strcmp((const char*)node->name, "stop") != 0)
 	{
-	  btrace("unexpected %s node",node->name);
-	  continue;	  
+	  btrace("unexpected %s node", node->name);
+	  continue;
 	}
 
       /* we have a stop node */
@@ -267,7 +267,7 @@ static int svg_read_lingrad(xmlNodePtr lgrad, svg_t* svg)
 
       /* offset is required */
 
-      if ((offset = xmlGetProp(stop,(const unsigned char*)"offset")) == NULL)
+      if ((offset = xmlGetProp(stop, (const unsigned char*)"offset")) == NULL)
 	{
 	  btrace("stop has no offset attribute, skipping");
 	  err++;
@@ -281,12 +281,12 @@ static int svg_read_lingrad(xmlNodePtr lgrad, svg_t* svg)
 	    {
 	      btrace("failed to parse offset \"%s\"", offset);
 	      err++;
-	    }      
+	    }
 	  else
 	    {
 	      xmlChar *colour, *opacity, *style;
 
-	      if ((style = xmlGetProp(stop,(const unsigned char*)"style")) != NULL)
+	      if ((style = xmlGetProp(stop, (const unsigned char*)"style")) != NULL)
 		{
 		  if (parse_style((char*)style, &rgb, &op) != 0)
 		    {
@@ -304,7 +304,7 @@ static int svg_read_lingrad(xmlNodePtr lgrad, svg_t* svg)
 		      btrace("failed on bad colour : %s", colour);
 		      err++;
 		    }
-		  
+
 		  xmlFree(colour);
 		}
 
@@ -315,7 +315,7 @@ static int svg_read_lingrad(xmlNodePtr lgrad, svg_t* svg)
 		      btrace("problem parsing opacity \"%s\"", opacity);
 		      err++;
 		    }
-		  
+
 		  xmlFree(opacity);
 		}
 
@@ -330,7 +330,7 @@ static int svg_read_lingrad(xmlNodePtr lgrad, svg_t* svg)
 		      err++;
 		    }
 		}
-	    }	
+	    }
 
 	  xmlFree(offset);
 
@@ -342,9 +342,9 @@ static int svg_read_lingrad(xmlNodePtr lgrad, svg_t* svg)
   return 0;
 }
 
-static int parse_offset(const char *po,double *z)
+static int parse_offset(const char *po, double *z)
 {
-  double x; 
+  double x;
 
   x = atof(po);
 
@@ -362,38 +362,38 @@ static int parse_offset(const char *po,double *z)
 
     <stop offset="0%" stop-color="white" stop-opacity="1">
 
-  seems the obvious thing to do. ho hum. 
+  seems the obvious thing to do. ho hum.
 
   This function writes the values in a style string into its
-  rgb and opacity argument if they are there, but does not touch 
+  rgb and opacity argument if they are there, but does not touch
   them otherwise. Neither case is an error.
 
-  We do a simple tokenisation with strtok_r, rather than 
+  We do a simple tokenisation with strtok_r, rather than
   introduce a dependency on a css parsing library
 */
 
-static int parse_style_statement(const char*,rgb_t*,double*);
+static int parse_style_statement(const char*, rgb_t*, double*);
 
-static int parse_style(const char *style,rgb_t* rgb,double* opacity)
+static int parse_style(const char *style, rgb_t* rgb, double* opacity)
 {
   size_t sz = strlen(style);
-  char buf[sz+1],*st,*state;
+  char buf[sz+1], *st, *state;
 
-  strcpy(buf,style);
+  strcpy(buf, style);
 
-  if ((st = strtok_r(buf,";",&state)) != NULL)
+  if ((st = strtok_r(buf, ";", &state)) != NULL)
     {
-      if (parse_style_statement(st,rgb,opacity) != 0)
+      if (parse_style_statement(st, rgb, opacity) != 0)
 	{
-	  btrace("failed on clause %s",st);
+	  btrace("failed on clause %s", st);
 	  return 1;
 	}
 
-      while ((st = strtok_r(NULL,";",&state)) != NULL)
+      while ((st = strtok_r(NULL, ";", &state)) != NULL)
 	{
-	  if (parse_style_statement(st,rgb,opacity) != 0)
+	  if (parse_style_statement(st, rgb, opacity) != 0)
 	    {
-	      btrace("failed on clause %s",st);
+	      btrace("failed on clause %s", st);
 	      return 1;
 	    }
 	}
@@ -405,27 +405,27 @@ static int parse_style(const char *style,rgb_t* rgb,double* opacity)
 static int parse_style_statement(const char *stmnt, rgb_t* rgb, double* opacity)
 {
   size_t sz = strlen(stmnt);
-  char buf[sz+1],*key,*val,*state;
+  char buf[sz+1], *key, *val, *state;
 
-  strcpy(buf,stmnt);
+  strcpy(buf, stmnt);
 
-  if ((key = strtok_r(buf,":",&state)) != NULL)
+  if ((key = strtok_r(buf, ":", &state)) != NULL)
     {
-      if ((val = strtok_r(NULL,":",&state)) != NULL)
+      if ((val = strtok_r(NULL, ":", &state)) != NULL)
 	{
-	  if (strcmp(key,"stop-color") == 0)
+	  if (strcmp(key, "stop-color") == 0)
 	    {
-	      if (parse_colour(val,rgb,opacity) != 0)
+	      if (parse_colour(val, rgb, opacity) != 0)
 		{
-		  btrace("stop-colour parse failed on %s [%s]",val,stmnt);
+		  btrace("stop-colour parse failed on %s [%s]", val, stmnt);
 		  return 1;
 		}
 	    }
-	  else if (strcmp(key,"stop-opacity") == 0)
+	  else if (strcmp(key, "stop-opacity") == 0)
 	    {
-	      if (parse_opacity(val,opacity) != 0)
+	      if (parse_opacity(val, opacity) != 0)
 		{
-		  btrace("opacity parse failed on %s",val);
+		  btrace("opacity parse failed on %s", val);
 		  return 1;
 		}
 	    }
@@ -440,17 +440,18 @@ static int parse_style_statement(const char *stmnt, rgb_t* rgb, double* opacity)
 static int parse_colour_numeric1(const char*);
 static int parse_colour_numeric2(const char*);
 
-static char* whitespace_trim(char*,int*); 
+static char* whitespace_trim(char*, int*);
 
 static int parse_colour(char *st, rgb_t *rgb, double *opacity)
 {
   const struct stdcol_t *p;
-  int r,g,b;
+  double op;
+  int r, g, b;
   int n = 0;
 
   if (st == NULL) return 1;
 
-  st = whitespace_trim(st,&n);
+  st = whitespace_trim(st, &n);
 
   if (n == 0) return 1;
 
@@ -463,7 +464,7 @@ static int parse_colour(char *st, rgb_t *rgb, double *opacity)
 	  r = parse_colour_numeric1(st+1);
 	  g = parse_colour_numeric1(st+2);
 	  b = parse_colour_numeric1(st+3);
-	  
+
 	  break;
 
 	case 7:
@@ -476,14 +477,14 @@ static int parse_colour(char *st, rgb_t *rgb, double *opacity)
 
 	default:
 
-	  btrace("bad hex colour %s",st);
+	  btrace("bad hex colour %s", st);
 
 	  return 1;
 	}
 
       if (r<0 || b<0 || g<0)
 	{
-	  btrace("bad hex colour %s",st);
+	  btrace("bad hex colour %s", st);
 
 	  return 1;
 	}
@@ -495,11 +496,34 @@ static int parse_colour(char *st, rgb_t *rgb, double *opacity)
       return 0;
     }
 
-  if (sscanf(st,"rgb(%i,%i,%i)",&r,&g,&b) == 3)
+  if (sscanf(st, "rgb(%i,%i,%i)", &r, &g, &b) == 3)
     {
       rgb->red   = r;
       rgb->green = g;
       rgb->blue  = b;
+
+      return 0;
+    }
+
+  /*
+    Apparently not yet fully standardized (though in alignment with the color
+    syntax in CSS3) which works in Firefox, Opera, Chrome and is occasionally
+    seen in the wild
+   */
+
+  if (sscanf(st, "rgba(%i,%i,%i,%lf)", &r, &g, &b, &op) == 4)
+    {
+      if ((op < 0) || (op > 1))
+	{
+	  btrace("bad opacity value %.4lf", op);
+	  return 1;
+	}
+
+      rgb->red   = r;
+      rgb->green = g;
+      rgb->blue  = b;
+
+      *opacity = op;
 
       return 0;
     }
@@ -518,9 +542,9 @@ static int parse_colour(char *st, rgb_t *rgb, double *opacity)
   return 1;
 }
 
-static char* whitespace_trim(char* st,int* pn)
+static char* whitespace_trim(char* st, int* pn)
 {
-  int i,n = strlen(st);
+  int i, n = strlen(st);
 
   for (i=0 ; i<n ; i++)
     {
@@ -558,7 +582,7 @@ static int parse_colour_numeric1(const char* st)
 
 static int parse_colour_numeric2(const char* st)
 {
-  int p,q,v;
+  int p, q, v;
 
   p = parse_hex(st[0]);
   q = parse_hex(st[1]);
@@ -579,17 +603,15 @@ static int parse_hex(char c)
   return -1;
 }
 
-static int parse_opacity(char *st,double *opacity)
+static int parse_opacity(char *st, double *opacity)
 {
   if (st == NULL) return 1;
 
   errno = 0;
 
-  *opacity = strtod(st,NULL);
+  *opacity = strtod(st, NULL);
 
   if (errno) return 1;
 
   return 0;
 }
-
-
